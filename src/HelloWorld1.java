25c337d270b0f21460b8ad112a433b618924a629
Tarun <Tarun@admin>
2019-03-30 06:59:26 +0530

    commit

---
+++ src/HelloWorld1.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld1
+{
+	{
+	int a=2;
+	int b=3;
+	int c=a+b;
+	System.out.println(c);
+}
+}
---
+++ src/HelloWorld2.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
