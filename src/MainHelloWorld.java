69c9c5c67af2457753e889eab7064ccb4787cb9a
Tarun <Tarun@admin>
2019-03-30 07:16:03 +0530

    commit

---
+++ src/MainHelloWorld.java
@@ -0,0 +1,16 @@
+
+public class MainHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println(c);
+		
+		
+
+	}
+
+}
